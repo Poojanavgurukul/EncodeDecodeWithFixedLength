fb02e6062911a8f0fe00a7104e7a7394a48fefcf
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-14 12:14:48 +0530

    add.Test Case for getting number of bits to represent

---
+++ src/test/java/EncodeDecodeTest/Bits.java
@@ -0,0 +1,13 @@
+package EncodeDecodeTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Bits {
+    @Test public void numberOfBitsInWhichUniqueCharacterRepresent(){
+        Bits bits=new Bits();
+        int expected =3;
+       assertEquals(expected,Bits);
+    }
+}
